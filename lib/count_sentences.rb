033f768ba80c9a700c5bec24d7aa8747f234c514
Michael Mottola <mottola64@gmail.com>
2019-04-04 00:48:08 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,11 +11,19 @@ class String
 end
 
   def question?
-
+if self.end_with?("?")
+  TRUE
+else
+  FALSE
+end
   end
 
   def exclamation?
-
+if self.end_with?("!")
+  TRUE
+else
+  FALSE
+end
   end
 
   def count_sentences
